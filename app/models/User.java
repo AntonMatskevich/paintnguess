e06e6383ea465ef2276084212d620acd3a6eada7
Anton_Matskevich <b02267@ut.ee>
2016-03-27 23:42:34 +0300

    DB magic #2

--- app/models/User.java
+++ app/models/User.java
@@ -1,14 +1,12 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by Anton on 25.03.16.
  */
 @Entity
+//@Table(name="user", schema = "user")
 public class User {
 
     @Id
--- build.sbt
+++ build.sbt
@@ -11,6 +11,8 @@ libraryDependencies ++= Seq(
   // JPA dependencies to avoid conflicts.
   javaJpa,
   javaJdbc,
+  "org.postgresql" % "postgresql" % "9.4-1200-jdbc4",
+  "org.hibernate.javax.persistence" % "hibernate-jpa-2.1-api" % "1.0.0.Final",
   "org.hibernate" % "hibernate-entitymanager" % "4.3.7.Final"
 )
 
