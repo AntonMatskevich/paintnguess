0141e27db0418808936d27d43f41f10f4fed8f31
AntonMatskevich <b02267@ut.ee>
2016-04-26 13:22:01 +0300

    finishing ordinar stuff (painter next)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -105,64 +105,9 @@ public class Application extends Controller {
         }
     }
 
-
     @Security.Authenticated(Secured.class)
     public static Result deletePlayer(Long id) {
         Player.delete(id);
-        return redirect(routes.Application.admin());
-    }
-
-    //room making
-    static Form<Room> roomForm = Form.form(Room.class);
-
-    @Security.Authenticated(Secured.class)
-    public static Result newRoom() {
-        if(Room.find.all().size() < 2) {
-            Form<Room> filledForm = roomForm.bindFromRequest();
-            if (filledForm.hasErrors()) {
-                return badRequest(
-                        views.html.pages.rooms.render(
-                                Player.find.byId(request().username())
-                        ));
-            } else {
-                Room.create(filledForm.get());
-                return redirect(routes.Application.rooms());
-            }
-        } else {
-            flash("room_error", "There is too many rooms");
-            return redirect(
-                    routes.Application.admin()
-            );
-        }
-    }
-
-    @Security.Authenticated(Secured.class)
-    public static Result deleteRoom(Long id) {
-        Room.delete(id);
-        return redirect(routes.Application.admin());
-    }
-
-
-    //team making
-    static Form<Team> teamForm = Form.form(Team.class);
-
-    @Security.Authenticated(Secured.class)
-    public static Result newTeam(Long id) {
-        Form<Team> filledForm = teamForm.bindFromRequest();
-        if(filledForm.hasErrors()) {
-            return badRequest(
-                    views.html.pages.rooms.render(
-                            Player.find.byId(request().username())
-                    ));
-        } else {
-            Team.create(filledForm.get(), id);
-            return redirect(routes.Application.rooms());
-        }
-    }
-
-    @Security.Authenticated(Secured.class)
-    public static Result deleteTeam(Long id) {
-        Team.delete(id);
         return redirect(routes.Application.admin());
     }
 
